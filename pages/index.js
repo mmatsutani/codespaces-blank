0220cf4f4334e3ad22a57fa3d1e7efb27789dfcb
Marcus Matsutani <matsutani@gmail.com>
2026-02-27 15:01:15 +0000

    só praticando..

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Greeting() {
     <>
       <h1>HIMYM ✨</h1>
       <p>Vocês ficarão chocados, crianças, quando descobrirem como é fácil se separar das pessoas pra sempre. É por isso que quando encontrar alguém que quer manter por perto, deve fazer algo a respeito.</p>
+      <address>Legen ... wait for it ... dary!</address>
     </>
   );
 }
