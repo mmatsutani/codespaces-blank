4387e0f31e55626115f839d1756c109f1f733eca
Marcus Matsutani <matsutani@gmail.com>
2026-02-27 14:59:04 +0000

    friendly heading

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Greeting() {
   return (
     <>
-      <h1>HIMYM</h1>
+      <h1>HIMYM ✨</h1>
       <p>Vocês ficarão chocados, crianças, quando descobrirem como é fácil se separar das pessoas pra sempre. É por isso que quando encontrar alguém que quer manter por perto, deve fazer algo a respeito.</p>
     </>
   );
